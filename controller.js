b50545a890105912d69573b909ea402b5348c7de
Maria Gabriela <o.alves15@alunoescolassociais.org.br>
2024-08-09 11:05:48 -0300

    ação excluir adicionado, ao clicar na opcão , o recurso (nomes ou dados) será exluido.

--- controller.js
+++ controller.js
@@ -34,13 +34,24 @@ function salvarUser() {
 function criarlista() {
   let tabela = document.getElementById('tabela').innerHTML = "<tr><th>nome Usuario</th><th>Ações</th</tr>";
   for (let i = 0; i <= (dadosLista.length - 1); i++) {
-    tabela += "<tr><td>" + dadosLista[i] + "</td><td>< button type='button'></td><tr>";
+    tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type='button'onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button type='button'onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td><tr>";
     document.getElementById('tabela').innerHTML = tabela;
   }
 }
 
-//FUNÇÃO PARA EDITAR NOMES LISTA
+// FUNÇÃO PARA EDITAR NOMES DA LISTA
 function editar(i){
   document.getElementById('nomeUser').value = dadosLista[(i - 1)];
   dadosLista.splice(dadosLista[(i - 1)], 1);
+}
+
+
+//FUNÇÃO PARA EDITAR NOMES LISTA
+function excluir(i){
+  //comentário sobre splice
+  dadosLista.splice(dadosLista[(i - 1)], 1);
+  document.getElementById('tabela').deleteRow(i);
+  document.getElementById('nomeUser').value = "";
+
+  
 }
